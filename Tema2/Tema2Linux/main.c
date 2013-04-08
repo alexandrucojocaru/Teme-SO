5d661dd8a46e2cd82479515192045f39c227f0ea
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2013-04-08 21:39:59 +0300

    First commit for Tema2 on Linux.

---
+++ Tema2/Tema2Linux/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 8, 2013
+ *      Author: Constantin Serban-Radoi 333CA
+ *
+ * 	Tema 2 SO
+ */
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+	printf("hello\n");
+
+	return 0;
+}
---
+++ Tema2/Tema2Linux/mpi.c
@@ -0,0 +1,12 @@
+/*
+ * mpi.c
+ *
+ *  Created on: Apr 8, 2013
+ *      Author: Constantin Serban-Radoi 333CA
+ *
+ * 	Tema 2 SO
+ */
+#include "mpi.h"
+#include "mpi_err.h"
+
+
